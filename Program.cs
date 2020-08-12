bbadf4c8cba6a8b6c2c572d48b8bbf2a22e433b0
ERIKASTIGLEMAN <69441647+ERIKASTIGLEMAN@users.noreply.github.com>
2020-08-12 16:49:30 -0400

    finished homework

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AllCardsOnDeckCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            // var hearts = new List<string>() { "❤️A", "❤️1", "❤️2", "❤️3", "❤️4", "❤️5", "❤️6", "❤️7", "❤️8", "❤️9", "❤️10", "❤️J", "❤️Q", "❤️K" };
+            var deck = new List<String>() { "♥︎A", "❤️1", "❤️2", "❤️3", "❤️4", "❤️5", "❤️6", "❤️7", "❤️8", "❤️9", "❤️10", "❤️J", "❤️Q", "❤️K", "♠︎A", "♠︎1", "♠︎2", "♠︎3", "♠︎4", "♠︎5", "♠︎6", "♠︎7", "♠︎8", "♠︎9", "♠︎10", "♠︎J", "♠︎Q", "♠︎K", "♣︎A", "♣︎1", "♣︎2", "♣︎3", "♣︎4", "♣︎5", "♣︎6", "♣︎7", "♣︎8", "♣︎9", "♣︎10", "♣︎J", "♣︎Q", "♣︎K", "♦︎A", "♦︎1", "♦︎2", "♦︎3", "♦︎4", "♦︎5", "♦︎6", "♦︎7", "♦︎8", "♦︎9", "♦︎10", "♦︎J", "♦︎Q", "♦︎K" };
+            var n = deck.Count();
+            var randomNumberGenerator = new Random();
+            for (int index = 0; index < n; index++)
+            {
+                var randomNumber = randomNumberGenerator.Next(0, n);
+                var placeholder = deck[randomNumber];
+                deck[randomNumber] = deck[index];
+                deck[index] = placeholder;
+            }
+            Console.WriteLine(deck[1]);
+            Console.WriteLine(deck[0]);
+
+
+
+
+
+        }
+
     }
-  }
 }
